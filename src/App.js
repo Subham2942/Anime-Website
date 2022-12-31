da4337b45c17a43c467e08d8a132af9e8d6f4e5f
Subham Das <subhamdassneha@gmail.com>
2023-01-01 00:41:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {useState, useEffect} from 'react'
+
+import SearchResults from './components/SearchResults.jsx'
+import SearchBar from "./components/SearchBar.jsx";
+
+import fetchAnime from './api/services'
+import './App.css'
+import Loading from "./components/Loading.jsx";
+import TrendingAnime from './components/TrendingAnime'
+import TopAnime from './components/TopAnime'
 
 function App() {
+
+
+    const [animeData, setAnimeData] = useState()
+    const [query, setQuery] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    
+    useEffect(() => {   
+        
+        const fetchData = async () =>{
+          setLoading(true);
+          await fetchAnime('anime', {q: query})
+          .then(resData => setAnimeData(resData.data))
+          .catch(setAnimeData(null))
+  
+          setLoading(false);
+      }
+      if(query)
+      {
+        fetchData();
+      }
+        
+        
+       
+    }, [query]);
+
+    // console.log(animeData && animeData.length === 0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <SearchBar setQuery={setQuery} />
+      <div className="container">
+         
+      {loading && <Loading/>}
+
+      {animeData && !loading && (<SearchResults results={animeData} query={query} />) }
+    
+      {(!loading && animeData && animeData.length === 0)  &&  <h5 onClick={()=> setAnimeData(null)}>No Anime Found</h5>}
+   
+
+      <TrendingAnime />
+      <TopAnime/>
     </div>
+    </>
+    
   );
 }
 
